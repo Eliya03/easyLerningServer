49cb94f9f9fcb035ac8a543069867b3c479fa615
shimon driham <95618903+shimondriham@users.noreply.github.com>
2024-10-13 14:46:52 +0300

    a

--- db/mongoConnect.js
+++ db/mongoConnect.js
@@ -7,7 +7,6 @@ async function main() {
   await mongoose.connect(
     // Your connection strings 
     `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@roie.i6xor.mongodb.net/easylarning`
-    // `mongodb+srv://roie:Aa123456@roie.i6xor.mongodb.net/easylarning`
   );
   console.log("mongo connect frome start_node");
 }

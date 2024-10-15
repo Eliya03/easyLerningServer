6a492120540d9756a99a6b64f291d90f3db800f0
2552123456 <vidal.judea@gmail.com>
2024-10-15 11:57:57 +0300

    aa

--- routes/chats.js
+++ routes/chats.js
@@ -3,7 +3,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", (req, res, next) => {
-  res.json({ msg: "Work from chats.js easy larning" });
+    res.json({ msg: "Work from chats.js easy larning" });
+});
+
+router.post("/", async (req, res) => {
+    try {
+
+
+        return res.json(data);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+router.get("/test1", async (req, res) => {
+    try {
+
+        return res.json(data);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 });
 
 module.exports = router;
